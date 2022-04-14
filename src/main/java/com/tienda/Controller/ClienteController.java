c0f8f4d5b1bec0bf01ef3b7bf507cd64d388a26e
sebaslizano7194 <slizano40347@ufide.ac.cr>
2022-04-13 20:11:56 -0600

    Tarea 4 - Jasper Reports
    Tarea 4 - Jasper Reports

--- src/main/java/com/tienda/Controller/ClienteController.java
+++ src/main/java/com/tienda/Controller/ClienteController.java
@@ -10,12 +10,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.tienda.Domain.Cliente;
+import com.tienda.Service.ClienteReportService;
 import com.tienda.Service.ClienteService;
 import com.tienda.dao.ClienteDao;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -28,18 +35,21 @@ public class ClienteController {
     @Autowired //Crea una instancia sin tener que llamarla
     private ClienteService clienteService;
 
+    @Autowired
+    private ClienteReportService clienteReportService;
+
     @GetMapping("/cliente/listado")
     public String inicio(Model model) {
-        var clientes=clienteService.getClientes();
-        
-        var limiteTotal=0;
-        for (var c: clientes) {
-            limiteTotal+=c.getCredito().getLimite();
+        var clientes = clienteService.getClientes();
+
+        var limiteTotal = 0;
+        for (var c : clientes) {
+            limiteTotal += c.getCredito().getLimite();
         }
-        model.addAttribute("limiteTotal",limiteTotal);
-        model.addAttribute("totalClientes",clientes.size());
-        
-        model.addAttribute("clientes",clientes);
+        model.addAttribute("limiteTotal", limiteTotal);
+        model.addAttribute("totalClientes", clientes.size());
+
+        model.addAttribute("clientes", clientes);
         return "/cliente/listado";
     }
 
@@ -47,24 +57,42 @@ public class ClienteController {
     public String nuevoCliente(Cliente cliente) {
         return "/cliente/modifica"; //Creamos  una nueva vista pues
     }
-    
+
     @PostMapping("/cliente/guardar")
-    public String guardarCliente(Cliente cliente){
+    public String guardarCliente(Cliente cliente) {
         clienteService.save(cliente);
         return "redirect:/cliente/listado"; //Aca estamos redireccionando a la accion default que esta arriba
     }
-    
+
     @GetMapping("/cliente/modificar/{idCliente}")
-    public String modificarCliente(Cliente cliente, Model model){
+    public String modificarCliente(Cliente cliente, Model model) {
         var respuesta = clienteService.getCliente(cliente);
         model.addAttribute("cliente", respuesta);
         return "/cliente/modifica";
     }
-    
-    @GetMapping ("/cliente/eliminar/{idCliente}")
-    public String eliminarCliente(Cliente cliente){
+
+    @GetMapping("/cliente/eliminar/{idCliente}")
+    public String eliminarCliente(Cliente cliente) {
         clienteService.delete(cliente);
         return "redirect:/cliente/listado";
+    }
+
+    @GetMapping(value = "/cliente/ReporteClientes", produces = MediaType.APPLICATION_PDF_VALUE)
+    public @ResponseBody
+    byte[] getFile() throws IOException {
+        try {
+            FileInputStream fis = new FileInputStream(new File(clienteReportService.generateReport()));
+            byte[] targetArray = new byte[fis.available()];
+            fis.read(targetArray);
+            return targetArray;
+        } catch (FileNotFoundException e) {
+// TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+// TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
